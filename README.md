3af51960728f1cafeaea807a946673cbf0bc686d
Elnur Nabivi <114493875+elnurnabivi@users.noreply.github.com>
2023-03-17 16:09:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Calculator
+
+This calculator application is built using HTML, CSS, and JavaScript. It is a simple yet functional calculator that can perform basic mathematical operations such as addition, subtraction, multiplication, and division. The interface is user-friendly and intuitive, making it easy for users to input their calculations and get accurate results. The application is responsive and can be used on various devices. It can be accessed by simply opening the HTML file in a web browser. Overall, this calculator is a useful tool for anyone who needs to perform quick and simple calculations.
+
+# Getting started
+Clone the repository: git clone https://github.com/elnurnabivi/Calculator
+
+# Built with
+HTML, Vanilla CSS, JavaScript
---
+++ app.js
@@ -0,0 +1,36 @@
+(function(){
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    })
+
+    equal.addEventListener('click', function(e){
+        if(screen.value === ''){
+            screen.value = "";
+        } else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+    }) 
+
+
+    /* equal.addEventListener('click', function(e){
+        try {
+            let answer = eval(screen.value);
+            screen.value = answer;
+        } catch (err) {
+            screen.value = "Error";
+        }
+    }) */
+
+    clear.addEventListener('click', function(e){
+        screen.value = "";
+    })
+})();
